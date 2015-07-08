b755bd2c26bb62ba04eff9073b70015852cef0fd
liz-0339 <elizahjkim@gmail.com>
2015-07-08 13:11:09 -0500

    Fixed

--- MergeFolderFilestoDataset.R
+++ MergeFolderFilestoDataset.R
@@ -1,27 +1,24 @@
-#This script takes all the files from one folder and merges them into one dataset
-#"dstest2" is the name of the compiled dataset, change this to desired name
+#setwd location to folder of all files
+#dsname is the name of compiled dataset
+#temp_dsname is the name of temporary dataset
 
 
 setwd("C:/Users/Blanche/Dropbox/LEGGO/PauseTimes/LizPT/")
-#set working directory to folder with all .csv files
 
 file_list <- list.files()
 
 for (file in file_list){
   
-  # if the merged dataset doesn't exist, create it
-  if (!exists("dstest2")){
-    dstest2<- read.csv(file, header=TRUE)
-    
+  # if the merged wtf doesn't exist, create it
+  if (!exists("dsname")){
+    dsname <- read.csv(file, header=TRUE)
   }
   
-  # if the merged dataset does exist, append to it
-  if (exists("datasetFIX")){
-    temp_dataset <-read.csv(file, header=TRUE)
-    dstest2<-rbind(dstest2, temp_dataset)
-    rm(temp_dataset)
+  # if the merged wtf does exist, append to it
+  if (exists("dsname")){
+    temp_dsname <-read.csv(file, header=TRUE)
+    dsname<-rbind(dsname, temp_dsname)
+    rm(temp_dsname)
   }
   
 }
-
-
