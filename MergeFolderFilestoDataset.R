06f1fad469b777df8855019ed15c5baf99efdd14
liz-0339 <elizahjkim@gmail.com>
2015-07-09 14:01:25 -0500

    FIXED officially

--- MergeFolderFilestoDataset.R
+++ MergeFolderFilestoDataset.R
@@ -1,24 +1,28 @@
 #setwd location to folder of all files
-#dsname is the name of compiled dataset
-#temp_dsname is the name of temporary dataset
+#testin4 is the name of compiled dataset
+#temp_testin4 is the name of temporary dataset
 
 
-setwd("C:/Users/Blanche/Dropbox/LEGGO/PauseTimes/LizPT/")
+setwd("C:/Users/Blanche/Dropbox/LEGGO/TestTimes/")
 
 file_list <- list.files()
 
+testin4 <- data.frame ()
+
+
 for (file in file_list){
   
-  # if the merged wtf doesn't exist, create it
-  if (!exists("dsname")){
-    dsname <- read.csv(file, header=TRUE)
+    # if the merged wtf doesn't exist, create it
+  if (!exists("testin4")){
+    testin4 <- read.csv(file, header=TRUE)
+    rm()
   }
   
   # if the merged wtf does exist, append to it
-  if (exists("dsname")){
-    temp_dsname <-read.csv(file, header=TRUE)
-    dsname<-rbind(dsname, temp_dsname)
-    rm(temp_dsname)
+  if (exists("testin4")){
+    temp_testin4 <-read.csv(file, header=TRUE)
+    testin4<-rbind(testin4, temp_testin4)
+    rm(temp_testin4)
   }
   
 }
