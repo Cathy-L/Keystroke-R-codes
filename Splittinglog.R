139281165d794b7cac8c3c02d8e46c7314af15c1
sydney-96 <sydney.tran@utexas.edu>
2015-07-27 10:38:11 -0500

    dont need the only | key subset

--- Splittinglog.R
+++ Splittinglog.R
@@ -4,7 +4,7 @@ test0 = xmlParse("C:/Users/STT/Downloads/Ek_20150723_15_GA.xml")
 rawtest0 <- getNodeSet(test0, "//session/event")
 test0_df = xmlToDataFrame(nodes = rawtest0)
 #onlytest0key <- subset(test0_df, type == "keyboard")
-onlyIkey <- subset(test0_df, output == "|")
+
 onlyparagraph <- test0_df[-c(as.numeric(row.names(subset(test0_df, output == "|")))+1:nrow(test0_df)),]
 keyparagraph <- subset(onlyparagraph, type == "keyboard")
 onlynatural <- test0_df[-c(1:row.names(subset(test0_df, output == "|"))),]
