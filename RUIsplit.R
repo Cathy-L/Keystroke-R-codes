8c90736b774cab5579d1473d9c19106a3bd5fada
sydney-96 <sydney.tran@utexas.edu>
2015-07-27 10:35:15 -0500

    Splitting the paragraph from natural on the RUI txt files

---
+++ RUIsplit.R
@@ -0,0 +1,10 @@
+setwd("C:/Users/STT/OneDrive/InputLogR")
+test2 <- read.table("test2.txt", header = FALSE, fill = TRUE) #makes new headers named v1, v2, etc..
+colnames(test2)[1] <- "Time"   #renames column names
+colnames(test2)[2] <- "Action"
+colnames(test2)[3] <- "output"
+onlytest2key <- subset(test2, Action == "KEY")
+onlypara <- test2[-c(as.numeric(row.names(subset(test2, output == "|")))+1:nrow(test2)),]
+onlyparakey <- subset(onlypara, Action == "KEY")
+onlynatl <- test2[-c(1:row.names(subset(test2, output == "|"))),]
+onlynatlkey <- subset(onlynatl, Action == "KEY")
