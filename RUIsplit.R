39f13db5668681e8ada0809b0d042691f2c2500f
sydney-96 <sydney.tran@utexas.edu>
2015-07-27 10:37:34 -0500

    don't need onlytest2key

--- RUIsplit.R
+++ RUIsplit.R
@@ -3,7 +3,7 @@ test2 <- read.table("test2.txt", header = FALSE, fill = TRUE) #makes new headers
 colnames(test2)[1] <- "Time"   #renames column names
 colnames(test2)[2] <- "Action"
 colnames(test2)[3] <- "output"
-onlytest2key <- subset(test2, Action == "KEY")
+
 onlypara <- test2[-c(as.numeric(row.names(subset(test2, output == "|")))+1:nrow(test2)),]
 onlyparakey <- subset(onlypara, Action == "KEY")
 onlynatl <- test2[-c(1:row.names(subset(test2, output == "|"))),]
