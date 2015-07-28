3a6c76a0ed7889c3453f6e4db104ab247b9b3842
sydney-96 <sydney.tran@utexas.edu>
2015-07-28 13:01:25 -0500

    alternative for cutting doc for inputlog

--- EndParaMarker.R
+++ EndParaMarker.R
@@ -4,4 +4,7 @@ whichtest = xmlParse("C:/Users/STT/Documents/InputLog/which/2015-07-28_0/output/
 rawwhich <- getNodeSet(whichtest, "//session/event")
 whichtest_df = xmlToDataFrame(nodes = rawwhich)
 whichtestkey <- subset(whichtest_df, type == "keyboard")
-endpara <- min(which(grepl(8, whichtest_df$output)))
+paragraph <- whichtest_df[c(1:min(which(grepl(8, whichtest_df$output)))), ]
+paragraphkey <- subset(paragraph, type == "keyboard") #have to subset keyboard after cause which returns diff row number if subset before
+natural <- whichtest_df[c(min(which(grepl(8, whichtest_df$output)))+1: nrow(whichtest_df)), ]
+naturalkey <- subset(natural, type == "keyboard")
